1ec54b230f24ea3302ed088eb55d384323a8d717
williamEstony <williamestony@gmail.com>
2020-04-15 23:31:41 -0400

    Implemented the main three requirements, as well as added a function to check for basic errors when the fetch request is made

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const url = 'https://cfw-takehome.developers.workers.dev/api/variants';
+  const key = 'variants'; //name of the key in the JSON object pointing to the array of urls value
+  try{
+    let resp = checkErrors(await fetch(url));
+    let json = await resp.json();
+    let urls = json[key];
+    return distRequests(urls);
+  }catch(error){
+    return new Response(error.message + ' Error', {
+      headers: { 'content-type': 'text/plain' },
+    })
+  }
+}
+
+async function distRequests(urls){
+  //Get a random number between 0 and 1 inclusive
+  //to represent the index of two possible variants
+  let variant = Math.floor(Math.random() * 2);
+  return await fetch(urls[variant]);
+}
+
+function checkErrors(request){
+  if (!request.ok) {
+    throw Error(request.status);
+  }else{
+    return request;
+  }
 }

d77596d9d4161d4e975de931e4b00a8683b5c080
williamEstony <williamestony@gmail.com>
2020-04-18 01:10:39 -0400

    completed customization of the variant pages, added functionality to point worker to my personal domain. Still needs cookies and clearer documentation

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+class ElementHandler {
+  constructor(styleMap) {
+    this.styleMap = styleMap;
+  }
+
+  element(element) {
+    let tag = element.tagName;
+    if(tag in this.styleMap && element.getAttribute('id') === this.styleMap[tag]['id']){
+      element.setInnerContent(this.styleMap[tag]['value']);
+      if(this.styleMap[tag].hasOwnProperty('attributes')){
+        for (var key in this.styleMap[tag]['attributes']) {
+          element.setAttribute(key, this.styleMap[tag]['attributes'][key]);
+        }
+      }
+    }
+  }
+}
+
 async function handleRequest(request) {
   const url = 'https://cfw-takehome.developers.workers.dev/api/variants';
   const key = 'variants'; //name of the key in the JSON object pointing to the array of urls value
@@ -21,7 +39,17 @@ async function distRequests(urls){
   //Get a random number between 0 and 1 inclusive
   //to represent the index of two possible variants
   let variant = Math.floor(Math.random() * 2);
-  return await fetch(urls[variant]);
+  let resp =  await fetch(urls[variant]);
+  const styleMap = getStyleMap();
+  return new HTMLRewriter().on('*', new ElementHandler(styleMap['variant' + variant])).transform(resp)
+}
+
+async function getStyleMap(){
+  let request = new Request("./styleMap.json");
+  await fetch(request)
+    .then(function(resp){
+      return resp.json();
+    })
 }
 
 function checkErrors(request){
@@ -29,5 +57,52 @@ function checkErrors(request){
     throw Error(request.status);
   }else{
     return request;
+  }
+}
+
+function getStyleMap(){
+  return {
+    'variant0': {
+      title: {
+        value: 'Will Estony\'s Website',
+        id: null
+      },
+      h1:{
+        value: 'Check out my new personal site',
+        id: 'title'
+      },
+      p:{
+        value: 'to learn more about my interests and experience',
+        id: 'description'
+      },
+      a: {
+        value: 'Go to my site',
+        id: 'url',
+        attributes: {
+          href: "http://cakephp-mysql-persistent-estony-ecs417.bde1.qmul-eecs.openshiftapps.com/website/willEstony.html"
+        }
+      }
+    },
+    'variant1': {
+      title: {
+        value: 'Will Estony\'s Favorite Movies',
+        id: null
+      },
+      h1:{
+        value: 'Here is a link to my top 5 favorite movies',
+        id: 'title'
+      },
+      p: {
+        value: 'and the leading actors who starred in them',
+        id: 'description'
+      },
+      a: {
+        value: 'Go to my movie list',
+        id: 'url',
+        attributes: {
+          href: "http://cakephp-mysql-persistent-estony-ecs417.bde1.qmul-eecs.openshiftapps.com/topic4/exercise2/exercise2.html"
+        }
+      }
+    }
   }
 }
